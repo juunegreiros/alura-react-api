faf254c4c537c0cb9f41c2949136f5485fdd426f
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2020-10-13 10:12:02 -0300

    1.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
+import Board from './pages/Board';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Board />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/pages/Board/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Board = () => <p>Aqui é o nosso board!</p>;
+
+export default Board;
